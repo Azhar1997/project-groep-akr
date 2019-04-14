0f67568d809112744e5b18a47c5510930bde4d8a
Robertowww <robnieuwendijk@hotmail.com>
2019-04-15 00:44:28 +0200

    Dit is een nieuwe versie met rectancle,triangle. Ik krijg alleen ovaal niet goed.
    En ook het hoofd van het poppetje gemaakt.

---
+++ main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        printStars.rectangle(7,15);
+        System.out.println("");
+        printStars.triangle(7,15);
+        System.out.println("");
+        printStars.ovale(7,15);
+        System.out.println("");
+        Poppetje.hoofd();
+    }
+}
+
